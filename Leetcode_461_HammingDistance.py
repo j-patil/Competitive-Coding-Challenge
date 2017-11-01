f0b68e13b2d2a1f807967ebedd21db9654e11102
j-patil <jayesh.patil1995@gmail.com>
2017-11-01 00:38:35 -0400

    Added comments

--- Leetcode_461_HammingDistance.py
+++ Leetcode_461_HammingDistance.py
@@ -12,6 +12,16 @@ class Solution:
         """
         binx = '{0:032b}'.format(x)
         biny = '{0:032b}'.format(y)
+        
+        """
+        (Stackoverflow) Just to explain the parts of the formatting string:
+
+        {} places a variable into a string
+        0 takes the variable at argument position 0
+        : adds formatting options for this variable (otherwise it would represent decimal x)
+        032 formats the number to thirty two digits zero-padded on the left
+        b converts the number to its binary representation
+        """
         count = 0
         for i, j in zip(binx, biny):
             if(i!=j):
