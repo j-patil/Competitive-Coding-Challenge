ce0806523d9b94234f7a9ab94cd5a87631b7f395
j-patil <jayesh.patil1995@gmail.com>
2017-11-03 04:11:21 -0400

    Reverse String code

---
+++ Leetcode_344_ReverseString.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 3, 2017
+
+@author: Jayesh
+'''
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
+    
+result = Solution()
+print(result.judgeCircle('Hello'))
