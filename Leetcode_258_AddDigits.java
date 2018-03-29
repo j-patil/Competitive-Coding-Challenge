821ca8fbdc1fe6f67816406d3de0fd4edc0336c6
Jayesh <jayeshpatil@nyu.edu>
2018-03-28 20:02:58 -0400

    Math formula

--- Leetcode_258_AddDigits.java
+++ Leetcode_258_AddDigits.java
@@ -1,14 +1,5 @@
 class Solution {
-    public int uniqueMorseRepresentations(String[] words) {
-        String[] arr = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        Set<String> set = new HashSet<>();
-        for(String s: words){
-            StringBuffer sb = new StringBuffer("");
-            for(int i=0; i<s.length(); i++){
-                sb.append(arr[s.charAt(i)-'a']);
-            }
-            set.add(String.valueOf(sb));
-        }
-        return set.size();
+    public int addDigits(int num) {
+        return ((num-1)%9) + 1;
     }
 }
