b56a2f98dbc42d67c277c4d81a20b944e1408eb9
Jayesh <jayeshpatil@nyu.edu>
2018-04-24 12:50:19 -0400

    Log n of square root

---
+++ Leetcode_69_Sqrt(x).java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mySqrt(int x) {
+        if((x==0) || (x==1)) return x;
+        long temp = x;
+        while(temp*temp > x){
+            temp = (temp + x/temp)/2;
+        }
+        return (int)temp;
+    }
+}
